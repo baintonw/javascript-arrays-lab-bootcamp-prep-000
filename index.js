1508e1fbbf2f94f2411e60163904b8f60f29dfaf
William Bainton <baintonw@gmail.com>
2019-04-05 12:53:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(element) {
+  var Ralph = element
+  kittens.push(Ralph)
+  return kittens
+}
+
+function destructivelyPrependKitten(element) {
+  var Ralph = element
+  kittens.unshift(Ralph)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(element) {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(element) {
+  kittens.shift()
+  return kittens
+}
+
